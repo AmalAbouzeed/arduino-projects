f3d48569728eee4d74742115fee1204832a71039
Amal Mahmoud Abouzeed <47079526+AmalAbouzeed@users.noreply.github.com>
2020-11-18 15:20:33 +0200

    Add files via upload

---
+++ push_button/push_button.ino
@@ -0,0 +1,27 @@
+//امال محمود السيد حامد جروب1
+
+void setup() {
+  //start serial connection
+  Serial.begin(9600);
+  //configure pin2 as an input and enable the internal pull-up resistor
+  pinMode(3, INPUT_PULLUP);
+  pinMode(2, OUTPUT);
+
+}
+
+void loop() {
+  //read the pushbutton value into a variable
+  int sensorVal = digitalRead(3);
+  //print out the value of the pushbutton
+  Serial.println(sensorVal);
+
+  // Keep in mind the pullup means the pushbutton's
+  // logic is inverted. It goes HIGH when it's open,
+  // and LOW when it's pressed. Turn on pin 13 when the
+  // button's pressed, and off when it's not:
+  if (sensorVal == HIGH) {
+    digitalWrite(2, HIGH);
+  } else {
+    digitalWrite(2, LOW);
+  }
+}
